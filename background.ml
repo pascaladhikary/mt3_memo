c9a424556269fe38e7ff7262211c714b79a86fb0
Pascal <adhikary.pascal@gmail.com>
2023-04-18 01:56:34 -0500

    Update background.ml

--- background.ml
+++ background.ml
@@ -4,8 +4,7 @@ let rec occurs v ty =
   match ty with TyVar x -> x = v | TyConst (y, z) -> List.exists (occurs v) z;;
 
 let rec subst_fun subst tyv =
-  match sigma with [] -> TyVar ty | (x, y)::z -> if x = ty then y else subst_fun z ty;;
+  match subst with [] -> TyVar tyv | (x, y)::z -> if x = tyv then y else subst_fun z tyv;;
 
 let rec monoTy_lift_subst sigma ty = 
   match ty with TyVar x -> subst_fun sigma x | TyConst (y, z) -> TyConst (y, List.map (monoTy_lift_subst sigma) z);;
-  
